83048c9a7e01bfef1b1f230e2b5f208ec3732d4e
Aoyagi Gakuto <k024g0082@g.neec.ac.jp>
2026-06-15 11:55:56 +0900

    Update level_editor.py

--- level_editor.py
+++ level_editor.py
@@ -30,81 +30,54 @@ class MYADDON_OT_export_scene(bpy.types.Operator, bpy_extras.io_utils.ExportHelp
     
     def parse_scene_recursive(self, file, object, level):
         """シーン解析用再帰関数"""
-        
-        #深さ分インデントする(タブを挿入)
-        indent = ''
-        for i in range(level):
-            indent += "\t"
+        indent = "\t" * level
 
-        # オブジェクト名書き込み
-        self.write_and_print(file, indent + object.type + " - " + object.name)
+        # 変更：オブジェクトのタイプのみを出力 (例: MESH)
+        self.write_and_print(file, indent + object.type)
+        
+        # トランスフォーム情報の抽出と変換
         trans, rot, scale = object.matrix_local.decompose()
-        # 回転を Quaternion から Euler (3軸での回転角)に変換
         rot = rot.to_euler()
-        # ラジアンから度数法に変換
         rot_x = math.degrees(rot.x)
         rot_y = math.degrees(rot.y)
         rot_z = math.degrees(rot.z)
-        # トランスフォーム情報を表示
-        self.write_and_print(file, indent + "Trans(%f,%f,%f)" % (trans.x, trans.y, trans.z))
-        self.write_and_print(file, indent + "Rot(%f,%f,%f)" % (rot_x, rot_y, rot_z))
-        self.write_and_print(file, indent + "Scale(%f,%f,%f)" % (scale.x, scale.y, scale.z))
-        self.write_and_print(file, "")
         
+        # 指定されたフォーマット (T, R, S) で出力
+        self.write_and_print(file, indent + "T %f %f %f" % (trans.x, trans.y, trans.z))
+        self.write_and_print(file, indent + "R %f %f %f" % (rot_x, rot_y, rot_z))
+        self.write_and_print(file, indent + "S %f %f %f" % (scale.x, scale.y, scale.z))
+        
+        # カスタムプロパティ 'file_name' があれば N として出力
+        if "file_name" in object:
+            self.write_and_print(file, indent + "N %s" % object["file_name"])
+            
+        # オブジェクトデータの終了を示す END を出力
+        self.write_and_print(file, indent + 'END')
+        self.write_and_print(file, '')
+        
+        # 子ノードへ進む（深さが1上がる）
         for child in object.children:
-            self.parse_scene_recursive(file, child, level +1)
+            self.parse_scene_recursive(file, child, level + 1)
 
     def export(self):
-        "ファイルに出力"
-
         print("シーン情報出力開始…%r"%self.filepath)
-
-        #ファイルをテキスト形式で書き出し用にオープン
         self.file = open(self.filepath, "wt", encoding="utf-8")
-
-        #ファイルに文字列を書き込む
         self.file.write("SCENE\n\n")
 
     def execute(self, context):
         print("シーン情報をExportします")
-        
-        #ファイルに出力
         self.export()
         
-        # シーン内のオブジェクト情報をループしてコンソルに出力
         for object in bpy.context.scene.objects:
-            
-            print(object.type + " - " + object.name)
-            #ローカルトランスフォーム行列から平行移動、回転、スケーリングを抽出
-            # 型はVector, Quaternion, Vector
-            trans, rot, scale = object.matrix_local.decompose()
-            #回転を Quternion から Euler (3軸での回転角)に変換
-            rot = rot.to_euler()
-            #ラジアンから度数法に変換
-            rot.x = math.degrees(rot.x)
-            rot.y = math.degrees(rot.y)
-            rot.z = math.degrees(rot.z)
-            
-            #トランスフォーム情報を表示
-            print("Trans(%f,%f,%f)" % (trans.x, trans.y, trans. z) )
-            print("Rot(%f,%f,%f)" % (rot.x, rot.y, rot.z) )
-            print("Scale(%f,%f,%f)" % (scale.x, scale.y, scale.z) )
-            #親オブジェクトの名前を表示
             if object.parent:
-                print("Parent:" + object.parent.name)
-            print()
-            
-            #親オブジェクトがあるものはスキップ(代わりに親から呼び出すから)
-            if (object.parent):
-               continue
-
-            #シーン直下のオブジェクトをルートノード(深さ0)とし、再帰関数で走査
+                continue
             self.parse_scene_recursive(self.file, object, 0)
             
         self.file.close()
         print("シーン情報をExportしました")
         self.report({'INFO'}, "シーン情報をExportしました")
         return {'FINISHED'}
+
 
 # ICO球を生成するオペレータークラス
 class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
@@ -113,65 +86,79 @@ class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
     bl_description = "ICO球を生成します"
     bl_options = {'REGISTER', 'UNDO'}
 
-    # メニューを実行したときに呼ばれる関数
     def execute(self, context):
         bpy.ops.mesh.primitive_ico_sphere_add()
         print("ICO球を生成しました。")
         return {'FINISHED'}
 
-# トップバーの拡張メニュー
-class TOPBAR_MT_my_menu(bpy.types.Menu):
-    # Blenderがクラスを識別する為の固有の文字列
-    bl_idname = "TOPBAR_MT_my_menu"
-    # メニューのラベルとして表示される文字列
-    bl_label = "MyMenu"
-    # 著者表示用の文字列
-    bl_description = "Level Editor - by " + bl_info["author"]
 
-    # サブメニューの描画
+# 追加：オペレータ カスタムプロパティ['file_name']追加
+class MYADDON_OT_add_filename(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_add_filename"
+    bl_label = "FileName 追加"
+    bl_description = "['file_name']カスタムプロパティを追加します"
+    bl_options = {"REGISTER", "UNDO"}
+
+    def execute(self, context):
+        context.object["file_name"] = ""
+        return {"FINISHED"}
+
+
+# オブジェクトのファイルネームパネルクラス
+class OBJECT_PT_file_name(bpy.types.Panel):
+    """オブジェクトのファイルネームパネル"""
+    bl_idname = "OBJECT_PT_file_name"
+    bl_label = "FileName"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "object"
+
     def draw(self, context):
         layout = self.layout
-        # トップバーの「エディターメニュー」に項目(オペレータ)を追加
-        layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
         
-        layout.separator()
-        
-        layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text="ICO球生成", icon='MESH_ICOSPHERE')
+        if "file_name" in context.object:
+            layout.prop(context.object, '["file_name"]', text=self.bl_label)
+        else:
+            layout.operator(MYADDON_OT_add_filename.bl_idname)
 
+
+# トップバーの拡張メニュー
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    bl_idname = "TOPBAR_MT_my_menu"
+    bl_label = "MyMenu"
+    bl_description = "Level Editor - by " + bl_info["author"]
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("wm.url_open_preset", text="Manual", icon='HELP')
+        layout.separator()
+        layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text="ICO球生成", icon='MESH_ICOSPHERE')
         layout.operator(MYADDON_OT_export_scene.bl_idname, text="シーン出力", icon='EXPORT')
 
-    # 既存のメニューにサブメニューを追加するための関数
     def submenu(self, context):
-        # ID指定でサブメニューを追加
         self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
 
 # 登録するクラスのリスト
 classes = (
-    MYADDON_OT_export_scene,
     MYADDON_OT_create_ico_sphere,
+    MYADDON_OT_export_scene,
     TOPBAR_MT_my_menu,
+    MYADDON_OT_add_filename,
+    OBJECT_PT_file_name,
 )
 
-# アドオン有効化時コールバック
 def register():
-    # クラスをBlenderに登録
     for cls in classes:
         bpy.utils.register_class(cls)
-    
-    # メニューに項目を追加（TOPBAR_MT_my_menuのsubmenuメソッドを登録）
     bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
     print("レベルエディタが有効化されました。")
 
-# アドオン無効化時コールバック
 def unregister():
-    # メニューから項目を削除
     bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
-    
-    # Blenderからクラスを削除
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
     print("レベルエディタが無効化されました。")
 
-# テスト用コード
 if __name__ == "__main__":
     register()
